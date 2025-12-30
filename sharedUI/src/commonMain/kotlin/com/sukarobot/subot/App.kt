76a160b54807cfb01d2ab3765d09ce690e97ccb3
arifamsar <dcylert@gmail.com>
2025-12-30 15:33:42 +0700

    Integrate Koin dependency injection into the main App entry point
    - Wrap `AppNavHost` with `KoinApplication` in `App.kt`.
    - Initialize Koin modules, specifically registering `preferencesModule` and `viewModelModule`.

--- sharedUI/src/commonMain/kotlin/com/sukarobot/subot/App.kt
+++ sharedUI/src/commonMain/kotlin/com/sukarobot/subot/App.kt
@@ -1,12 +1,21 @@
 package com.sukarobot.subot
 
 import androidx.compose.runtime.Composable
+import com.subot.core.data.di.preferencesModule
 import com.sukarobot.subot.navigation.AppNavHost
 import com.sukarobot.subot.theme.AppTheme
+import com.sukarobot.subot.ui.di.viewModelModule
+import org.koin.compose.KoinApplication
 
 @Composable
 fun App(
     onThemeChanged: @Composable (isDark: Boolean) -> Unit = {}
 ) = AppTheme(onThemeChanged) {
-    AppNavHost()
+    KoinApplication(
+        application = {
+            modules(preferencesModule, viewModelModule)
+        }
+    ) {
+        AppNavHost()
+    }
 }
