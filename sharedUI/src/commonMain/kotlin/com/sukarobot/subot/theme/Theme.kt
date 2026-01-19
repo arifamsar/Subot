e8eb63b311236be9cedd6f83117b33d41e2b62b4
arifamsar <dcylert@gmail.com>
2026-01-19 15:36:53 +0700

    Cleanup import of Theme.kt

--- sharedUI/src/commonMain/kotlin/com/sukarobot/subot/theme/Theme.kt
+++ sharedUI/src/commonMain/kotlin/com/sukarobot/subot/theme/Theme.kt
@@ -1,12 +1,16 @@
 package com.sukarobot.subot.theme
 
-import androidx.compose.foundation.LocalIndication
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 private val LightColorScheme = lightColorScheme(
     primary = PrimaryLight,
@@ -97,8 +101,7 @@ internal fun AppTheme(
         isDarkState.value = isDark
     }
     CompositionLocalProvider(
-        LocalThemeIsDark provides isDarkState,
-        LocalIndication provides ScaleIndication
+        LocalThemeIsDark provides isDarkState
     ) {
         val currentIsDark by isDarkState
         onThemeChanged(currentIsDark)
