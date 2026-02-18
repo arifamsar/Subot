4eaec622726594d28b5f43bf3ab5e88fbeca236f
arifamsar <dcylert@gmail.com>
2026-02-18 11:46:51 +0700

    feat: Add liquid effect to `AppTopBar`
    This commit integrates the `liquid` library to apply a visual effect to the `AppTopBar`.
    
    - Import `liquid` and related dependencies.
    - Conditionally apply the `liquid` modifier to the `TopAppBar` when the `isLiquidEnabled()` flag is true.
    - Configure the liquid effect with a `CutCornerShape`, frost, curve, refraction, and dispersion.

--- core/ui/src/commonMain/kotlin/com/subot/core/ui/components/AppTopBar.kt
+++ core/ui/src/commonMain/kotlin/com/subot/core/ui/components/AppTopBar.kt
@@ -1,8 +1,7 @@
 package com.subot.core.ui.components
 
 import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Icon
@@ -16,19 +15,24 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import com.subot.core.ui.components.icons.ArrowLeft
 import com.subot.core.ui.components.icons.Hicon
+import com.subot.core.ui.isLiquidEnabled
+import io.github.fletchmckee.liquid.liquid
+import io.github.fletchmckee.liquid.rememberLiquidState
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun AppTopBar(
-    title: String,
     modifier: Modifier = Modifier,
+    title: String,
     navigationIcon: ImageVector = Hicon.ArrowLeft,
     onNavigationClick: (() -> Unit)? = null,
     windowInsets: WindowInsets = TopAppBarDefaults.windowInsets,
-    actions: @Composable () -> Unit = {}
+    actions: @Composable () -> Unit = {},
 ) {
+    val liquidTopBarState = rememberLiquidState()
     TopAppBar(
         title = {
             Text(
@@ -36,7 +40,17 @@ fun AppTopBar(
                 style = MaterialTheme.typography.titleLarge
             )
         },
-        modifier = modifier,
+        modifier = modifier.then(
+            if (isLiquidEnabled()) {
+                Modifier.liquid(liquidState = liquidTopBarState) {
+                    this.shape = CutCornerShape(0.dp)
+                    this.frost = 8.dp
+                    this.curve = .4f
+                    this.refraction = .1f
+                    this.dispersion = .2f
+                }
+            } else Modifier
+        ),
         navigationIcon = {
             if (onNavigationClick != null) {
                 IconButton(
