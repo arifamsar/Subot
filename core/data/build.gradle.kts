bd611a636a77d917c6fa5616df99768943831fe8
arifamsar <dcylert@gmail.com>
2026-02-27 18:39:01 +0700

    feat: Add `BuildConfig` for API base URL
    This commit introduces the `BuildConfig` plugin to manage environment-specific variables, starting with the API's `BASE_URL`.
    
    - Add the `buildConfig` plugin to `core/data/build.gradle.kts`.
    - Configure `BuildConfig` to read the `BASE_URL` property from the root project's `local.properties` file.
    - Expose the `BASE_URL` as a `buildConfigField`, making it accessible within the data module's source code.

--- core/data/build.gradle.kts
+++ core/data/build.gradle.kts
@@ -1,9 +1,21 @@
 import com.subot.convention.utils.coreDataDependencies
+import org.jetbrains.compose.internal.utils.localPropertiesFile
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.subot.kotlin.multiplatform)
     alias(libs.plugins.subot.room)
     alias(libs.plugins.kotlinx.serialization)
+    alias(libs.plugins.buildConfig)
+}
+
+buildConfig {
+    val localPropertiesFile = rootProject.localPropertiesFile
+    val properties = Properties().apply {
+        localPropertiesFile.inputStream().use { load(it) }
+    }
+    val  baseUrl: String = properties.getProperty("BASE_URL")
+    buildConfigField("BASE_URL", baseUrl )
 }
 
 kotlin {
