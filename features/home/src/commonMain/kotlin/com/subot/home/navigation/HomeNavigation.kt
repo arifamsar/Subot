b995b3e455b7a5613c334f6a51caf1317d0761a5
arifamsar <dcylert@gmail.com>
2026-03-30 18:28:12 +0700

    refactor: Add `SharedTransitionScope` to `homeFlow` navigation
    This commit updates the `homeFlow` entry provider to accept a `SharedTransitionScope`, enabling the use of shared element transitions within the home navigation flow.
    
    - **Update `homeFlow` signature:**
        - Add `sharedTransitionScope: SharedTransitionScope` as a required parameter.
        - Prepare the `HomeScreen` entry to support transition animations.

--- features/home/src/commonMain/kotlin/com/subot/home/navigation/HomeNavigation.kt
+++ features/home/src/commonMain/kotlin/com/subot/home/navigation/HomeNavigation.kt
@@ -1,5 +1,6 @@
 package com.subot.home.navigation
 
+import androidx.compose.animation.SharedTransitionScope
 import androidx.navigation3.runtime.EntryProviderScope
 import androidx.navigation3.runtime.NavKey
 import com.subot.core.ui.navigation.Navigator
@@ -8,6 +9,7 @@ import com.subot.home.screens.home.HomeScreen
 
 fun EntryProviderScope<NavKey>.homeFlow(
     navigator: Navigator,
+    sharedTransitionScope: SharedTransitionScope,
 ) {
     entry<Route.Home> {
         HomeScreen()
